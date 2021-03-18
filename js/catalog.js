647938bfa2ec36944931b2abb2f31475be039faa
Basel Mousa Atalla <baselatalla10@gmail.com>
2021-03-18 11:25:35 +0200

    lab-14

---
+++ js/catalog.js
@@ -0,0 +1,118 @@
+/* global Product, Cart */
+
+'use strict';
+
+// Set up an empty cart for use on this page.
+var cart = new Cart([]);
+
+// On screen load, we call this method to put all of the busmall options
+// (the things in the Product.allProducts array) into the drop down list.
+function populateForm() {
+
+  //todo: Add an <option> tag inside the form's select for each product
+  var selectEl = document.getElementById('items');
+
+  for (var i in Product.allProducts) {
+    var optionEl = document.createElement('option');
+    optionEl.textContent = Product.allProducts[i].name;
+    selectEl.appendChild(optionEl);
+  }
+
+}
+
+// When someone submits the form, we need to add the selected item to the cart
+// object, save the whole thing back to local storage and update the screen
+// so that it shows the # of items in the cart and a quick preview of the cart itself.
+function handleSubmit(event) {
+
+  //todo: Prevent the page from reloading
+  event.preventDefault();
+
+
+  // Do all the things ...
+  addSelectedItemToCart();
+  cart.saveToLocalStorage();
+  updateCounter();
+  updateCartPreview();
+
+}
+
+// todo: Add the selected item and quantity to the cart
+function addSelectedItemToCart() {
+  // todo: suss out the item picked from the select list
+  var selectItem = event.target.items.value;
+
+  // todo: get the quantity
+  var selectAmount = event.target.quantity.value;
+
+  // todo: using those, add one item to the Cart
+  console.log(selectItem, selectAmount);
+
+  cart.addItem(selectItem, selectAmount);
+
+}
+
+// todo: Update the cart count in the header nav with the number of items in the Cart
+function updateCounter() {
+  var cartEl = document.getElementById('itemCount');
+  var cartTotal = 0;
+  cart.items = JSON.parse(localStorage.getItem('cart'));
+  for (var i in cart.items) {
+    cartTotal += parseInt(cart.items[i].quantity);
+  }
+  cartEl.textContent = ` (${cartTotal})`;
+}
+
+// todo: As you add items into the cart, show them (item & quantity) in the cart preview div
+function updateCartPreview() {
+  // todo: Get the item and quantity from the form
+  var selectItem = event.target.items.value;
+  var selectAmount = event.target.quantity.value;
+
+  console.log(`display this: ${selectItem}, ${selectAmount}`);
+
+  // todo: Add a new element to the cartContents div with that information
+  var divEl = document.getElementById('cartContents');
+  var tableEl = document.getElementById('cartTable');
+  var trEl = document.createElement('tr');
+  var thEl = document.createElement('th');
+  var tdEl = document.createElement('td');
+
+  if (document.getElementById('cartTable')) {
+    trEl = document.createElement('tr');
+    tdEl.textContent = 'IMG';
+    trEl.appendChild(tdEl);
+    trEl = document.createElement('tr');
+    tdEl.textContent = selectItem;
+    trEl.appendChild(tdEl);
+    tdEl = document.createElement('td');
+    tdEl.textContent = selectAmount;
+    trEl.appendChild(tdEl);
+    tableEl.appendChild(trEl);
+    divEl.appendChild(tableEl);
+  } else {
+    var newtableEl = document.createElement('table');
+    newtableEl.id = 'cartTable';
+    trEl = document.createElement('tr');
+    thEl = document.createElement('th');
+    tdEl = document.createElement('td');
+    thEl = document.createElement('th');
+    thEl.textContent = 'ITEM '
+    trEl.appendChild(thEl);
+    thEl = document.createElement('th');
+    thEl.textContent = ' QTY';
+    trEl.appendChild(thEl);
+    newtableEl.appendChild(trEl);
+    divEl.appendChild(newtableEl);
+  }
+}
+
+// Set up the "submit" event listener on the form.
+// This is the trigger for the app. When a user "submits" the form, it will
+// Call that handleSubmit method above and kick off the whole process
+var catalogForm = document.getElementById('catalog');
+catalogForm.addEventListener('submit', handleSubmit);
+
+// Before anything else of value can happen, we need to fill in the select
+// drop down list in the form.
+populateForm();
